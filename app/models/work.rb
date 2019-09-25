6edb0de3e0dba878e62a9280a28f89f34d9dac80
Adam Wead <amsterdamos@gmail.com>
2019-09-25 15:46:11 -0400

    Fleshing out the metadata on the work
    Adds the have_jsonb_accessor RSpec matcher for easier testing.

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,7 +4,20 @@ class Work < ApplicationRecord
   jsonb_accessor :metadata,
                  title: :string,
                  subtitle: :string,
-                 keywords: [:string, array: true, default: []]
+                 keywords: [:string, array: true, default: []],
+                 rights: :string,
+                 description: :string,
+                 resource_type: :string,
+                 contributor: :string,
+                 publisher: :string,
+                 published_date: :string,
+                 subject: :string,
+                 language: :string,
+                 identifier: :string,
+                 based_near: :string,
+                 related_url: :string,
+                 source: :string
+
   belongs_to :depositor, class_name: 'User', foreign_key: 'depositor_id', inverse_of: 'works'
   has_many :work_creations, dependent: :restrict_with_exception
   has_many :aliases, through: :work_creations
