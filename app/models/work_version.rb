59e1df554d5d586bc0050ff6d1ff4e5b89012f2c
Adam Wead <amsterdamos@gmail.com>
2019-09-24 22:43:48 -0400

    Sketching out workflow

--- app/models/work_version.rb
+++ app/models/work_version.rb
@@ -1,7 +1,33 @@
 # frozen_string_literal: true
 
 class WorkVersion < ApplicationRecord
+  include AASM
+
+  aasm do
+    state :draft, intial: true
+    state :published, :withdrawn, :removed
+
+    event :publish do
+      before do
+        # whatever we need to do...
+      end
+      transitions from: [:draft, :withdrawn], to: :published, guard: :valid_for_publication?
+    end
+
+    event :withdraw do
+      transitions from: :published, to: :withdrawn
+    end
+
+    event :remove do
+      transitions from: [:draft, :withdrawn], to: :removed
+    end
+  end
+
   belongs_to :work
   has_many :version_memberships, dependent: :destroy
   has_many :file_resources, through: :version_memberships
+
+  def valid_for_publication?
+    true
+  end
 end
--- spec/models/work_version_spec.rb
+++ spec/models/work_version_spec.rb
@@ -7,11 +7,21 @@ RSpec.describe WorkVersion, type: :model do
     it { is_expected.to have_db_column(:work_id) }
     it { is_expected.to have_db_column(:title) }
     it { is_expected.to have_db_index(:work_id) }
+    it { is_expected.to have_db_column(:aasm_state) }
   end
 
   describe 'associations' do
     it { is_expected.to belong_to(:work) }
     it { is_expected.to have_many(:version_memberships) }
     it { is_expected.to have_many(:file_resources).through(:version_memberships) }
+  end
+
+  describe 'states' do
+    it { is_expected.to have_state(:draft) }
+    it { is_expected.to transition_from(:draft).to(:published).on_event(:publish) }
+    it { is_expected.to transition_from(:published).to(:withdrawn).on_event(:withdraw) }
+    it { is_expected.to transition_from(:withdrawn).to(:published).on_event(:publish) }
+    it { is_expected.to transition_from(:draft).to(:removed).on_event(:remove) }
+    it { is_expected.to transition_from(:withdrawn).to(:removed).on_event(:remove) }
   end
 end
