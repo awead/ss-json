f02f6833dcc68bee9e8864168cd8b483bf1b7ec3
Adam Wead <amsterdamos@gmail.com>
2019-09-25 15:46:11 -0400

    Fleshing out the metadata on the work
    Adds the have_jsonb_accessor RSpec matcher for easier testing.

--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -7,6 +7,21 @@ RSpec.describe Work, type: :model do
     it { is_expected.to have_db_column(:metadata).of_type(:jsonb) }
     it { is_expected.to have_db_column(:depositor_id) }
     it { is_expected.to have_db_index(:depositor_id) }
+    it { is_expected.to have_jsonb_accessor(:title).of_type(:string) }
+    it { is_expected.to have_jsonb_accessor(:subtitle).of_type(:string) }
+    it { is_expected.to have_jsonb_accessor(:keywords).of_type(:string).is_array.with_default([]) }
+    it { is_expected.to have_jsonb_accessor(:rights).of_type(:string) }
+    it { is_expected.to have_jsonb_accessor(:description).of_type(:string) }
+    it { is_expected.to have_jsonb_accessor(:resource_type).of_type(:string) }
+    it { is_expected.to have_jsonb_accessor(:contributor).of_type(:string) }
+    it { is_expected.to have_jsonb_accessor(:publisher).of_type(:string) }
+    it { is_expected.to have_jsonb_accessor(:published_date).of_type(:string) }
+    it { is_expected.to have_jsonb_accessor(:subject).of_type(:string) }
+    it { is_expected.to have_jsonb_accessor(:language).of_type(:string) }
+    it { is_expected.to have_jsonb_accessor(:identifier).of_type(:string) }
+    it { is_expected.to have_jsonb_accessor(:based_near).of_type(:string) }
+    it { is_expected.to have_jsonb_accessor(:related_url).of_type(:string) }
+    it { is_expected.to have_jsonb_accessor(:source).of_type(:string) }
   end
 
   describe 'associations' do
@@ -14,31 +29,5 @@ RSpec.describe Work, type: :model do
     it { is_expected.to have_many(:work_creations) }
     it { is_expected.to have_many(:aliases).through(:work_creations) }
     it { is_expected.to have_many(:work_versions) }
-  end
-
-  describe '#title' do
-    subject { described_class.new(title: 'My title') }
-
-    its(:title) { is_expected.to eq('My title') }
-  end
-
-  describe '#subtitle' do
-    subject { described_class.new(subtitle: 'My subtitle') }
-
-    its(:subtitle) { is_expected.to eq('My subtitle') }
-  end
-
-  describe '#keywords' do
-    context 'without any keywords' do
-      subject { described_class.new }
-
-      its(:keywords) { is_expected.to be_empty }
-    end
-
-    context 'with keywords' do
-      subject { described_class.new(keywords: ['keyword1', 'keyword2']) }
-
-      its(:keywords) { is_expected.to contain_exactly('keyword1', 'keyword2') }
-    end
   end
 end
