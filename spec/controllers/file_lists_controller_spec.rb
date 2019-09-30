ac5ba5ee37a7ac7c273ac5a5169fb5b5ffb41049
Adam Wead <amsterdamos@gmail.com>
2019-09-30 15:40:26 -0400

    FileListsController

--- spec/controllers/file_lists_controller_spec.rb
+++ spec/controllers/file_lists_controller_spec.rb
@@ -3,4 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe FileListsController, type: :controller do
+  let(:valid_session) { {} }
+  let(:work_version) { User.create.works.create.versions.create }
+
+  describe '#edit' do
+    it 'returns a success response' do
+      get :edit, params: { work_id: work_version.work.id, version_id: work_version.id }, session: valid_session
+      expect(response).to be_successful
+    end
+  end
+
+  describe '#update' do
+    it 'redirects somewhere' do
+    end
+  end
 end
