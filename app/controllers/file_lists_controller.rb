57183225cc8a68fd775d91835ac27dba29fc4fb3
Adam Wead <amsterdamos@gmail.com>
2019-09-30 15:40:26 -0400

    FileListsController

--- app/controllers/file_lists_controller.rb
+++ app/controllers/file_lists_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class FileListsController < ApplicationController
+  before_action :load_work_version
+
+  def edit; end
+
+  def update; end
+
+  private
+
+    def load_work_version
+      @work_version = WorkVersion.find(params[:version_id])
+    end
 end
