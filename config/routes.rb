63ff967fe4c98d4696abb527e0585af70ffafd7f
Adam Wead <amsterdamos@gmail.com>
2019-09-30 15:25:09 -0400

    Routes for FileLists

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@
 
 Rails.application.routes.draw do
   resources :works do
-    resources :versions, controller: :work_versions # ,  shallow: true ???
+    resources :versions, controller: :work_versions do # ,  shallow: true ???
+      get 'file_list', to: 'file_lists#edit'
+      put 'file_list', to: 'file_lists#update'
+      patch 'file_list', to: 'file_lists#update'
+    end
   end
 end
