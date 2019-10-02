88bcbcad0fdb04c5a51c04980004f7ec68a36aaa
Ryan Schenk <rschenk@gmail.com>
2019-10-02 12:04:16 -0400

    File listings all use edited filename

--- app/models/file_version_membership.rb
+++ app/models/file_version_membership.rb
@@ -4,7 +4,15 @@ class FileVersionMembership < ApplicationRecord
   belongs_to :work_version
   belongs_to :file_resource
 
+  delegate :size, :mime_type, :original_filename, to: :uploader
+
   def title
-    read_attribute(:title).presence || file_resource&.file&.original_filename
+    read_attribute(:title).presence || original_filename
   end
+
+  private
+
+    def uploader
+      @uploader ||= file_resource&.file
+    end
 end
