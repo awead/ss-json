84e1a97812a07c28d8839afd67fee2b3d31b5311
Ryan Schenk <rschenk@gmail.com>
2019-10-02 12:04:33 -0400

    BuildNewVersion persists edited filenames

--- app/services/build_new_version.rb
+++ app/services/build_new_version.rb
@@ -3,12 +3,18 @@
 class BuildNewVersion
   def self.call(previous_version)
     parent_work = previous_version.work
+
     new_version = parent_work.versions.build(
       metadata: previous_version.metadata,
       aasm_state: 'draft'
     )
 
-    new_version.file_resources = previous_version.file_resources
+    previous_version.file_version_memberships.each do |previous_membership|
+      new_version.file_version_memberships.build(
+        file_resource_id: previous_membership.file_resource_id,
+        title: previous_membership.title
+      )
+    end
 
     new_version
   end
