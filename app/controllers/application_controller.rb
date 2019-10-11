70eb5a8b05c55606f696c11975b5ef86d65a9675
Dann Bohn <djb44@psu.edu>
2019-10-11 10:40:09 -0400

    remote current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,24 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-    def psu_oauth_client
-        @psu_oauth_client ||= OAuth2::Client.new(
-          ENV.fetch("DOORKEEPER_APP_ID"),
-          ENV.fetch("DOORKEEPER_APP_SECRET"),
-          site: ENV.fetch("DOORKEEPER_APP_URL")
-        )
-        puts @psu_oauth_client
-      end
-    
-      def psu_access_token
-        @psu_access_token ||=
-          if current_user
-            OAuth2::AccessToken.new(
-              psu_oauth_client, current_user.psu_access_token
-            )
-          end
-      end
   include Pundit
 
-  helper_method :current_user
-  def current_user
-    @current_user ||= (User.all.first || User.create)
-  end
+  private
+    def psu_oauth_client
+      @psu_oauth_client ||= OAuth2::Client.new(
+        ENV.fetch("OAUTH_APP_ID"),
+        ENV.fetch("OAUTH_APP_SECRET"),
+        site: ENV.fetch("OAUTH_APP_URL")
+      )
+      puts @psu_oauth_client
+    end
+
+    def psu_access_token
+      @psu_access_token ||=
+      if current_user
+        OAuth2::AccessToken.new(
+          psu_oauth_client, current_user.psu_access_token
+        )
+      end
+    end
 end
